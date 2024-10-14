49cb3f70bdfb2661e1d8f9858115aa6594d40e0e
Elanchezhiyan P <elanche97@gmail.com>
2024-10-14 10:55:00 +0530

    Update YOECalculator.py

--- YOECalculator.py
+++ YOECalculator.py
@@ -1,7 +1,7 @@
 import datetime
 
 # Set your starting year of experience
-start_date = datetime.datetime(2019, 8, 1)  # Change to your actual start date
+start_date = datetime.datetime(2015, 1, 1)  # Change to your actual start date
 current_date = datetime.datetime.now()
 
 # Calculate total months of experience
@@ -9,7 +9,7 @@ total_months = (current_date.year - start_date.year) * 12 + (current_date.month 
 
 # Calculate years in decimal format
 years_of_experience = total_months / 12
-formatted_experience = f"{years_of_experience:.1f} years of experience"
+formatted_experience = f"{years_of_experience:.1f}"
 
 # Update the README.md directly
 readme_path = 'README.md'
@@ -21,7 +21,7 @@ with open(readme_path, 'r') as f:
 # Update the line with the placeholder
 for i, line in enumerate(readme_content):
     if '<!-- years_of_experience_placeholder -->' in line:
-        readme_content[i] = f"## Experience\n\n{formatted_experience}\n\n"
+        readme_content[i] = line.replace('<!-- years_of_experience_placeholder -->', formatted_experience)
 
 # Write the updated content back to README.md
 with open(readme_path, 'w') as f:
